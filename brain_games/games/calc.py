b110fbca4dc30ea0a59f1c2351b23f07b971dce3
LliriKnat <petrunin.k.o@gmail.com>
2022-10-14 20:42:03 +1000

    minor calc-game refactoring
    Change eval() to operator(add, sub, mul) for calculating expressions in calc-game.
    Create is_even() function in even-game.

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,12 +1,17 @@
 import random
+import operator
 
 
 RULES = "What is the result of the expression?"
 
 
 def generate_game():
-    operands = ["+", "-", "*"]  # can create a dict with lambda functions
+    operation = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul
+    }
     nums = (random.randint(1, 10), random.randint(1, 10))
-    operand = operands[random.randint(0, 2)]
-    answer = eval(f"{nums[0]} {operand} {nums[1]}")
+    operand = random.choice(["+", "-", "*"])
+    answer = (operation[operand](nums[0], nums[1]))
     return (f"{nums[0]} {operand} {nums[1]}", str(answer))
--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,9 +4,16 @@ import random
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
 def generate_game():
     number = random.randint(1, 100)
-    if number % 2 == 0:
+    if is_even(number):
         return (f"{number}", "yes")
     else:
         return (f"{number}", "no")
